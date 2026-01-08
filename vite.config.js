a6ae033cd7c9bf6c4f1c4c71fc95f78511b7e225
wilsontt <wilsontt@icloud.com>
2026-01-08 14:50:49 +0800

    修改設定

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 產品、開發中 的路徑䛀定
+  // :的前面是 正式環境，：的後面是 開發中環境。
+    base: process.env.NODE_ENV === 'production' ? '/react-gh-pages-sample/' : '/',
   plugins: [
     react(),
     tailwindcss(),
